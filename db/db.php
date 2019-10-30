71b206ce9ebd5f8202627831905fb8b4058bf94c
jminota <jminota@pjminotap.plastilene.dns>
2019-10-30 18:03:45 -0500

    se arregla el comentario

--- db/db.php
+++ db/db.php
@@ -4,7 +4,7 @@ class Conectar{
         $conexion=new mysqli("remotemysql.com", "7tK58L6A24", "4iULrSz7Xe", "7tK58L6A24");
         $conexion->query("SET NAMES 'utf8'");
            
-             
+         echo "Hola este es un mensaje nuevo";    
         return $conexion;
     }
 }
