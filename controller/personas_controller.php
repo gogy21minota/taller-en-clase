ee10944d1562fd7beadc6c860159ff2450069715
jminota <jminota@pjminotap.plastilene.dns>
2019-10-30 18:06:10 -0500

    se arregla el comentario para el archivo controller

--- controller/personas_controller.php
+++ controller/personas_controller.php
@@ -3,6 +3,8 @@
 require_once("model/personas_model.php");
 $per=new personas_model();
 $datos=$per->get_personas();
+
+echo "Hola este es un mensaje nuevo";   
  
 //Llamada a la vista
 require_once("view/personas_view.phtml");
