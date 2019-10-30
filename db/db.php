31e75596d53f3ecaca3c7f368903dc694ccce5ce
jminota <jminota@pjminotap.plastilene.dns>
2019-10-30 15:54:21 -0500

    Inicio de codigo2

--- db/db.php
+++ db/db.php
@@ -3,6 +3,8 @@ class Conectar{
     public static function conexion(){
         $conexion=new mysqli("remotemysql.com", "7tK58L6A24", "4iULrSz7Xe", "7tK58L6A24");
         $conexion->query("SET NAMES 'utf8'");
+           
+             
         return $conexion;
     }
 }
